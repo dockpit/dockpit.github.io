404b8e13fa038f763d766c14fc5cd2d693978015
Ad van der Veer <advanderveer@gmail.com>
2015-05-21 17:54:16 +0200

    added Dockpit to the title

--- src/jsx/components/callout.jsx
+++ src/jsx/components/callout.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 import UIScreenshot from './ui_screenshot.jsx'
 import Navbar from './navbar.jsx'
- 
+
 class Callout extends React.Component {
     constructor(props) { super(props) }
 
@@ -11,22 +11,22 @@ class Callout extends React.Component {
 
 		  <Navbar/>
 
-		  <div className="container">		  	
-		  	<img className="logo" src="src/img/logo.png"/>	
+		  <div className="container">
+        <h1 style={{position: "absolute", "left": 0, "right": 0, "top": 170, "paddingLeft": 130}}>Dockpit</h1>
+		  	<img style={{marginLeft: "-150px"}} className="logo" src="src/img/logo.png"/>
 		    <p className="lead">{this.props.catchPhrase}</p>
-		    
+
 		    <a id="btn-download-callout" href="download.html" className="btn btn-default">
 		    	{this.props.btnCTA}&nbsp;&nbsp;<i className="fa fa-chevron-right"></i>
 		    </a>
-		    
-		    <p className="text-muted">(Free and open-source)</p>			    
+
+		    <p className="text-muted">(Free and open-source)</p>
 
 		    <ul className="platforms">
 		    	<li><img src="src/img/osx.png"/></li>
 		    	<li><img src="src/img/linux.png"/></li>
 		    	<li><img src="src/img/windows.png"/></li>
 		    </ul>
-
 		  </div>
 
 		  <UIScreenshot/>
